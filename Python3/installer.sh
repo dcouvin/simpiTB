290e210682f678ba3087834e828dca6787797322
dcazenav <92384123+dcazenav@users.noreply.github.com>
2023-08-01 16:23:11 -0400

    Update installer.sh

--- Python3/installer.sh
+++ Python3/installer.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 conda install -c conda-forge mamba
-
 conda install -c bioconda miru-hero
 conda install -c bioconda emboss
-conda activate tb-profiler
 
 mamba create -n tb-profiler -c conda-forge -c bioconda docxtpl tb-profiler=4.4.2
+conda activate tb-profiler
 
 git clone https://github.com/phglab/MIRUReader.git
 git clone https://github.com/matnguyen/SpoTyping.git
